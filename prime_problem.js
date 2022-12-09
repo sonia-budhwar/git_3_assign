ebf1172d94cc3f5740057cc662dc62e0c7d4ffb8
sonia-budhwar <budhwar1999sonia15@gmail.com>
2022-12-09 15:09:29 +0530

    sonia

---
+++ prime_problem.js
@@ -0,0 +1,15 @@
+let a=23;
+let prime=true;
+for(let i=2;i<=a;i++)
+{
+    if(a%i=0)
+    {
+        prime=false;
+    }
+}
+if(prime){
+    console.log("prime");
+}
+else{
+    console.log("not prime");
+}
